af3806f3f8fdd3a3fc05b01ab08b525ba1902eb5
Cuong Nguyen <cuog2805@gmail.com>
2025-07-24 11:16:47 +0700

    sửa validate file input

--- SMS_TYNB/wwwroot/js/file/file.js
+++ SMS_TYNB/wwwroot/js/file/file.js
@@ -1,12 +1,12 @@
 ﻿$(document).ready(function () {
 	// xử lý file upload modal
 	let debounceTimer;
-	$("#searchInput").on("input", function () {
+	$("#searchFileInput").on("input", function () {
 		clearTimeout(debounceTimer);
 
 		debounceTimer = setTimeout(function () {
-			currentPagination.pageNumber = 1;
-			loadData();
+			currentFilePagination.pageNumber = 1;
+			LoadFileInput();
 		}, 300);
 	});
 
@@ -14,9 +14,15 @@
 		LoadFileInput();
     });
 
-    $("#uploadNewFileBtn").on("click", function () {
-		$('#inputFileModal').modal('hide');
-		displaySelectedFiles();
+	$("#uploadNewFileBtn").on("click", function () {
+		if (!$('#uploadFileModalForm').data('validator')) {
+			initFileFormValidate();
+		}
+
+		if ($("#uploadFileModalForm").valid()) {
+			$('#inputFileModal').modal('hide');
+			displaySelectedFiles();
+		}
     });
 });
 
@@ -33,7 +39,7 @@ let paginationFileData = {
 let selectedFiles = [];
 
 function LoadFileInput() {
-	let searchInput = $('#searchInput').val();
+	let searchInput = $('#searchFileInput').val();
 	let pageable = {
 		pageNumber: currentFilePagination.pageNumber,
 		pageSize: currentFilePagination.pageSize,
@@ -158,4 +164,23 @@ function handleItemSelect(checkbox) {
 	} else {
 		selectedFiles = selectedFiles.filter(selected => selected.IdFile !== item.IdFile);
 	}
+}
+
+function initFileFormValidate() {
+    $('#uploadFileModalForm').validate({
+        rules: {
+            'FileDinhKem': {
+                extension: "doc|docx|pdf|png|jpg",
+                filesize: 5 * 1024 * 1024
+            }
+        },
+        messages: {
+            'FileDinhKem': {
+                extension: "Chỉ chấp nhận .doc, .docx, .pdf, .png, .jpg",
+                filesize: "Kích thước mỗi tệp không được vượt quá 5MB"
+            }
+        },
+        errorClass: "text-danger",
+        errorElement: "div",
+    });
 }
